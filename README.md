8967e886d867c2c3ab1249f6e6eb5899bb22fc92
Daniel <dbarton8406@gmail.com>
2015-09-28 01:42:34 -0400

    completed easy mode for tictactoe

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 # My TicTacToe game.
-Deliverables
+#Deliverables
 
-A script tic-tac-toe.rb
-Requirements
+#A script tic-tac-toe.rb
+#Requirements
 
-Running ruby tic-tac-toe.rb should play a game of tic-tac-toe in the terminal.
-Easy Mode
+#Running ruby tic-tac-toe.rb should play a game of tic-tac-toe in the terminal.
+#Easy Mode
 
-The script should play a game of tic-tac-toe to completion and print the game's outcome. Each turn, the script displays the current board and prompts the user for a move. It may either have two human players or a human player and a computer player that chooses a move randomly.
+#The script should play a game of tic-tac-toe to completion and print the game's outcome. Each turn, the script displays the current board and prompts the user for a move. It may either have two human players or a human player and a computer player that chooses a move randomly.
 
-Normal Mode
+#The script should fulfill the Easy Mode requirements and support both a human and computer second player.
 
-The script should fulfill the Easy Mode requirements and support both a human and computer second player.
+#Hard Mode
 
-Hard Mode
+#The AI should try to do something intelligent instead of choosing random empty spaces. Hardcoding "if the board is this, play this" is tractable, but there are several valid approaches.
 
-The AI should try to do something intelligent instead of choosing random empty spaces. Hardcoding "if the board is this, play this" is tractable, but there are several valid approaches.
+#Nightmare Mode
 
-Nightmare Mode
-
-Make it so that the computer will never lose.
-
+#Make it so that the computer will never lose.
---
+++ tictactoe.rb
@@ -0,0 +1,172 @@
+require "set"
+require "pry"
+
+$win_set = Set.new([Set.new([0,1,2]),Set.new([3,4,5]),
+                    Set.new([6,7,8]),Set.new([0,3,6]),Set.new([1,4,7]),
+                    Set.new([2,7,8]),Set.new([0,4,8]),Set.new([2,4,6])])
+
+
+
+def play
+  play_tictactoe
+  # choice = play_again?
+  # until choice == "n"
+  #   play_tictactoe
+  #   choice = play_again?
+end
+
+# def play_again?
+#   puts "Would you like to play again? (y/n)"
+#   gets.chomp
+# end
+
+def play_tictactoe
+  tictactoe
+end
+
+def tictactoe
+  turn_count = 9
+  board = (0..8).to_a
+  puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  puts "================================================================================"
+  puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  x = player_opponent
+  puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  puts "================================================================================"
+  puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  o = player_opponent
+  g1 = Set.new
+  g2 = Set.new
+  greeting
+  guesses = g1
+  current_player = x
+  until game_over?(guesses,turn_count)
+    puts current_player
+    guess = take_turn(guesses,board)
+    turn_count -= 1
+    guesses.add(guess)
+
+    if current_player == x
+      board[guess] = "X"
+      current_player = o
+      guesses = g2
+    else
+      current_player = x
+      board[guess] = "O"
+      guesses =g1
+    end
+  end
+  postmortem(guesses, board)
+end
+
+def player_opponent
+  "Is it a game... or is it real?\n\n".each_char {|c| putc c ;
+  sleep 0.05; $stdout.flush }
+  "What you see on these screens up here is a fantasy;
+  a computer enhanced hallucination!\n\n".each_char {|c| putc c ;
+  sleep 0.05; $stdout.flush }
+  puts"Name ?"
+  gets.chomp
+
+end
+
+def greeting
+
+  "\n\n\nGreetings, Dr.Falken \n\n".each_char {|c| putc c ;
+  sleep 0.05; $stdout.flush }
+  "shall we play a game?\n\n".each_char {|c| putc c ;
+  sleep 0.05; $stdout.flush }
+  "Global Thermonuclear War? \n\n ".each_char {|c| putc c ;
+  sleep 0.05; $stdout.flush }
+  "No,just tic tac toe.\n\n\n\n\n\n".each_char {|c| putc c ;
+  sleep 0.05; $stdout.flush }
+end
+
+def game_over?(guesses,turn_count )
+  turn_count == 0 || win?(guesses)
+end
+
+def win?(guesses)
+  $win_set.any? {|x| x.subset?(guesses)}
+
+end
+
+def take_turn(guesses, board)
+  show_board(board)
+  prompt_player(board)
+end
+
+def show_board(board)
+  puts "
+ #{board[0]} | #{board[1]} | #{board[2]}
+--- --- ---
+ #{board[3]} | #{board[4]} | #{board[5]}
+--- --- ---
+ #{board[6]} | #{board[7]} | #{board[8]}
+       \n\n"
+end
+
+def prompt_player(board)
+  puts
+  puts "Please choose a square: \n\n\n"
+  choice = gets.chomp
+  until valid_number?(choice.to_i) || !already_guessed?(board,choice>to_i)
+    puts "#{choice} is not a valid option.
+    Please choose again: "
+    choice = gets.chomp
+  end
+  choice.to_i
+end
+def valid_number?(choice)
+  if choice >= 0 && choice <= 8
+    true
+  else
+    false
+  end
+
+end
+def already_guessed?(board,choice)
+  board[choice] == "X" || board[choice] == "O"
+end
+
+def postmortem( guesses, board)
+  if win?(guesses)
+    "what is the primary goal?\n\n".each_char {|c| putc c
+    sleep 0.05; $stdout.flush }
+    "You should know, Professor you programmed me.\n\n".each_char {|c| putc c
+    sleep 0.05; $stdout.flush }
+    "To win the game.\n\n".each_char {|c| putc c
+    sleep 0.25; $stdout.flush }
+     puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    puts "================================================================================"
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+  else
+    "Greetings, Professor Falken\n\n".each_char {|c| putc c
+    sleep 0.05; $stdout.flush }
+    "Hello,Joshua.\n\n".each_char {|c| putc c
+    sleep 0.25; $stdout.flush }
+    " A strange game.\n\n".each_char {|c| putc c
+    sleep 0.05; $stdout.flush }
+    "The only winning move is not to play.\n\n".each_char {|c| putc c
+    sleep 0.05; $stdout.flush }
+    "How about a nice game of chess?.".each_char {|c| putc c
+    sleep 0.05; $stdout.flush }
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    puts "================================================================================"
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
+  end
+end
+# def play_again?
+#   puts "Would you like to play again? (y/n)"
+#   gets.chomp
+# end
+
+def play
+  play_tictactoe
+  # choice = play_again?
+  # until choice == "n"
+  #   play_tictactoe
+  #   choice = play_again?
+end
+play
